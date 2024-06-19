e11bc407563b383060c9f143a548197f7b66bd30
Egor <relax221b@gmail.com>
2024-06-19 20:24:23 +0300

    урок 2 wolf

---
+++ les 2/WolfTest.java
@@ -0,0 +1,8 @@
+public class WolfTest{
+    public static void main(String[] args){
+        Wolf wolf1 = new Wolf();
+        wolf1.nickname="Шарик";
+        wolf1.walk();
+        wolf1.sit();
+    }
+}
