627c35718400e91e1d3ff0ecd700f6b99d995f15
Egor <relax221b@gmail.com>
2024-06-29 21:57:46 +0500

    del folders

--- les 2/WolfTest.java
+++
@@ -1,9 +0,0 @@
-public class WolfTest{
-    public static void main(String[] args){
-        Wolf wolf1 = new Wolf();
-        wolf1.setAge(7);
-        System.out.println(wolf1.getAge());
-    }
-}
-
-
