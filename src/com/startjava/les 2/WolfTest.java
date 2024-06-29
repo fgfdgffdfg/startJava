cdb8377ba56c4d847d6d7aae80e80475e939a615
Egor <relax221b@gmail.com>
2024-06-29 21:52:39 +0500

    add src and out

---
+++ src/com/startjava/les 2/WolfTest.java
@@ -0,0 +1,9 @@
+public class WolfTest{
+    public static void main(String[] args){
+        Wolf wolf1 = new Wolf();
+        wolf1.setAge(7);
+        System.out.println(wolf1.getAge());
+    }
+}
+
+
